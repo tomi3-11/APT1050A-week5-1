0ef0a682da9a16f5410f6f22ec5dea29e4459edb
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-06 17:10:15 +0300

    Updated class work

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -13,3 +13,15 @@ USE [APT1050A]
 
 SELECT * FROM tProduct
 WHERE ProductID IN (select distinct ProductID FROM tOrderDetail where OrderQty > 3)
+
+SELECT ProductID, ProductName
+FROM tProduct
+
+WHERE ProductID IN (select ProductID
+					FROM tOrderDetail
+					WHERE SalesOrderID IN ( select SalesOrderID
+											FROM tOrder
+											WHERE CustomerID IN (select CustID
+															  FROM tCust
+															  WHERE city = 'seattle')))
+					
