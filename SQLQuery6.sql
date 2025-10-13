e7bb86e79ae326a522ec3fb291a4bf76abf15a3e
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-13 16:04:14 +0300

    Week6- Using SUM, GROUP BY, AVG

---
+++ SQLQuery6.sql
@@ -0,0 +1,26 @@
+USE [APT1050A]
+
+SELECT sum(OrderQty) As TotalQty
+From tOrderDetail
+
+SELECT count (productID) As TotalRecords
+From tProduct
+
+-- For product Grouped by product IDs generally
+SELECT ProductID, SUM(OrderQty) As TotalQty
+FROM tOrderDetail
+GROUP BY ProductID
+ORDER BY ProductID asc
+
+-- Totals that go with particular product
+SELECT ProductID, SUM(OrderQty) As TotalQty
+FROM tOrderDetail
+WHERE ProductID = 410
+GROUP BY ProductID
+--ORDER BY ProductID asc
+
+-- Average of order quantity
+SELECT ProductID, AVG(OrderQty) As AvgQty
+FROM tOrderDetail
+GROUP BY ProductID
+ORDER BY ProductID asc
